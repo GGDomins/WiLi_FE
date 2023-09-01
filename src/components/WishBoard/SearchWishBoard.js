8fc8c7859a7a00a678035586a4b1090aab74fd72
Seungwoo Kim <svvoo31o@gmail.com>
2023-09-01 18:57:57 +0200

    Fix: search logic

--- src/components/WishBoard/SearchWishBoard.js
+++ src/components/WishBoard/SearchWishBoard.js
@@ -14,46 +14,49 @@ const SearchWishBoard = () => {
   const [searchProducts, setSearchProducts] = useState([]);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const [state, refetch] = useAsync(() => searchProductReq(query), []);
-  const { loading, data, error } = state;
-
   useEffect(() => {
-    if (data) {
-      if (data.data.message === '유저 없음') {
-        setErrorMessage('존재하지 않는 유저입니다.');
-        return;
-      } else if (data.data.message === '제품 없음') {
-        setErrorMessage('존재하지 않는 제품입니다.');
-        return;
+    const fetchData = async () => {
+      const data = await searchProductReq(query);
+
+      if (data) {
+        if (data.data.message === '유저 없음') {
+          setErrorMessage('존재하지 않는 유저입니다.');
+          return;
+        } else if (data.data.message === '제품 없음') {
+          setErrorMessage('존재하지 않는 제품입니다.');
+          return;
+        }
+        setErrorMessage('');
+
+        const images = data.data.images;
+        const products = data.data.posts;
+
+        console.log(images);
+        console.log(products);
+
+        let imageUrls = images.map((base64String) => {
+          return 'data:image/png;base64,' + base64String;
+        });
+
+        const productObjs = products.map((product, i) => {
+          const parsed = JSON.parse(product);
+
+          return {
+            id: parsed.id,
+            brandName: parsed.brandName,
+            productName: parsed.productName,
+            img: imageUrls[i],
+          };
+        });
+
+        setSearchProducts(productObjs);
+
+        console.log(searchProducts);
       }
-      setErrorMessage('');
+    };
 
-      const images = data.data.images;
-      const products = data.data.posts;
-
-      console.log(images);
-      console.log(products);
-
-      let imageUrls = images.map((base64String) => {
-        return 'data:image/png;base64,' + base64String;
-      });
-
-      const productObjs = products.map((product, i) => {
-        const parsed = JSON.parse(product);
-
-        return {
-          id: parsed.id,
-          brandName: parsed.brandName,
-          productName: parsed.productName,
-          img: imageUrls[i],
-        };
-      });
-
-      setSearchProducts(productObjs);
-
-      console.log(searchProducts);
-    }
-  }, [data, query]);
+    fetchData();
+  }, [query]);
 
   return (
     <>
