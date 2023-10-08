24fa5b37d3f82257e5353973295051beebd1f3dd
Seungwoo Kim <svvoo31o@gmail.com>
2023-10-08 13:45:53 +0100

    Chore: removed previous code comments

--- src/utils/authAPIs/authAPIs.js
+++ src/utils/authAPIs/authAPIs.js
@@ -30,28 +30,6 @@ export const logoutReq = async () => {
 export const authReq = async () => {
   const response = await axiosInstance.post(`/users/auth`);
   return response;
-  // axios.defaults.withCredentials = true;
-
-  // const accessToken = localStorage.getItem('accessToken');
-
-  // if (accessToken) {
-  //   axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
-  // } else {
-  //   axios.defaults.headers.common['Authorization'] = null;
-  // }
-
-  // const response = await axios.post(
-  //   `${process.env.REACT_APP_SERVER_URL}/users/auth`,
-  //   {
-  //     headers: {
-  //       Authorization: accessToken,
-  //       'Access-Control-Allow-Origin': '*',
-  //       'Content-Type': 'application/json',
-  //     },
-  //   }
-  // );
-
-  // return response;
 };
 
 export const refreshReq = async () => {
@@ -65,22 +43,6 @@ export const refreshReq = async () => {
   } catch (error) {
     return false;
   }
-  // axios.defaults.withCredentials = true;
-
-  // try {
-  //   const response = await axios.post(
-  //     `${process.env.REACT_APP_SERVER_URL}/users/refresh-token`
-  //   );
-
-  //   const accessToken = response.headers['accesstoken'];
-  //   localStorage.setItem('accessToken', accessToken);
-
-  //   console.log('true');
-  //   return true;
-  // } catch (error) {
-  //   console.log(error);
-  //   return false;
-  // }
 };
 
 export const signUpReq = async (userInfo) => {
@@ -94,26 +56,4 @@ export const signUpReq = async (userInfo) => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   axios.withCredentials = true;
-
-  //   const response = await axios.post(
-  //     `${process.env.REACT_APP_SERVER_URL}/users/signup`,
-  //     userInfo,
-  //     {
-  //       headers: {
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //       },
-  //     }
-  //   );
-
-  //   const accessToken = response.headers['accesstoken'];
-  //   console.log(accessToken);
-  //   localStorage.setItem('accessToken', accessToken);
-
-  //   window.location.href = '/';
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
--- src/utils/productAPIs/productAPIs.js
+++ src/utils/productAPIs/productAPIs.js
@@ -8,22 +8,6 @@ export const productReq = async () => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const response = await axios.get(
-  //     `${process.env.REACT_APP_SERVER_URL}/users/products`,
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //       },
-  //     }
-  //   );
-
-  //   return response;
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const randomProductReq = async () => {
@@ -33,22 +17,6 @@ export const randomProductReq = async () => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const response = await axios.get(
-  //     `${process.env.REACT_APP_SERVER_URL}/random-feed`,
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //       },
-  //     }
-  //   );
-
-  //   return response;
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const searchProductReq = async (query) => {
@@ -58,22 +26,6 @@ export const searchProductReq = async (query) => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const response = await axios.get(
-  //     `${process.env.REACT_APP_SERVER_URL}/search?query=${query}`,
-  //     {
-  //       headers: {
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  //       },
-  //     }
-  //   );
-
-  //   return response;
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const addProductReq = async (formData) => {
@@ -87,26 +39,6 @@ export const addProductReq = async (formData) => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   axios.withCredentials = true;
-  //   console.log('uploading to database...');
-  //   const response = await axios.post(
-  //     `${process.env.REACT_APP_SERVER_URL}/products/add`,
-  //     formData,
-  //     {
-  //       headers: {
-  //         'Content-Type': 'multipart/form-data',
-  //         'Access-Control-Allow-Origin': '*',
-  //         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  //       },
-  //     }
-  //   );
-  //   console.log(response);
-  //   console.log('upload successful');
-  //   window.location.href = '/';
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const editProductReq = async (productInfo, id) => {
@@ -118,24 +50,6 @@ export const editProductReq = async (productInfo, id) => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const response = await axios.patch(
-  //     `${process.env.REACT_APP_SERVER_URL}/products/${id}`,
-  //     productInfo,
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //       },
-  //     }
-  //   );
-
-  //   alert('수정이 완료되었습니다.');
-  //   window.location.href = '/';
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const productInfoReq = async (id) => {
@@ -145,22 +59,6 @@ export const productInfoReq = async (id) => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const response = await axios.get(
-  //     `${process.env.REACT_APP_SERVER_URL}/products/${id}`,
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //       },
-  //     }
-  //   );
-
-  //   return response;
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const deleteProductReq = async (id) => {
@@ -173,22 +71,4 @@ export const deleteProductReq = async (id) => {
   } catch (error) {
     console.log(error);
   }
-  // try {
-  //   const response = await axios.delete(
-  //     `${process.env.REACT_APP_SERVER_URL}/products/${id}`,
-  //     {
-  //       headers: {
-  //         Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-  //         'Access-Control-Allow-Origin': '*',
-  //         'Content-Type': 'application/json',
-  //       },
-  //     }
-  //   );
-  //   console.log(response);
-
-  //   alert('삭제가 완료되었습니다.');
-  //   window.location.reload();
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
