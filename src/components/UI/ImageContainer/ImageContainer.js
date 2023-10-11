88a350f43b61066d6e36bc319003ec9742551ecc
Seungwoo Kim <svvoo31o@gmail.com>
2023-10-11 16:16:40 +0100

    Design: wishboard responsive layout

--- src/components/UI/ImageContainer/ImageContainer.js
+++ src/components/UI/ImageContainer/ImageContainer.js
@@ -13,6 +13,15 @@ const StyledContainer = styled.div`
   justify-content: center;
   background-color: 'black';
 
+  @media (max-width: 1400px) {
+    width: 1000px;
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 1080px) {
+    width: 700px;
+  }
+
   @media (max-width: 768px) {
     width: 380px;
     grid-template-columns: repeat(2, 1fr);
