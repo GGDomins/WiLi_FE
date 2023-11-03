e3b9d7a48e3ce89060e7073eb8c13cff0abbdee1
Seungwoo Kim <svvoo31o@gmail.com>
2023-11-03 23:31:33 +0000

    Feat: normal login added

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
 import { normalLoginReq } from '../../utils/authAPIs/authAPIs';
 import { StyledContainer, LoginContainer } from './style';
 
